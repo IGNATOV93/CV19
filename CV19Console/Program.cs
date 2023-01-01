2b58f06b3bd97c75b912347a0652b3c9c97b74e8
IGNATOV93 <ignatowja@gmail.com>
2023-01-01 21:14:49 +0300

    добавил папку тест и консольное приложение в нем

---
+++ CV19Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace CV19Console
+{
+    public class Progtam
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
