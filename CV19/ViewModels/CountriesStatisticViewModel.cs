a0a68430dff6c5c4d1ab3a88641a3aa6fbe88fba
IGNATOV93 <ignatowja@gmail.com>
2023-02-04 02:03:08 +0300

    Базовая логика модели-представления

--- CV19/ViewModels/CountriesStatisticViewModel.cs
+++ CV19/ViewModels/CountriesStatisticViewModel.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using CV19.Infrastructure.Commands;
+using CV19.Models;
+using CV19.Services;
 using CV19.ViewModels;
 using CV19.ViewModels.Base;
 
@@ -10,11 +14,44 @@ namespace CV19.View
 {
     internal class CountriesStatisticViewModel:ViewModel
     {
-        private readonly MainWindowViewModel _MainModel;
+        private DataService _DataService;
+
+        private  MainWindowViewModel _MainModel { get; }
+
+        #region Contries : IEnumerable<CountryInfo> - Статистика по странам
+        /// <summary> Статистика по странам </summary>
+        private IEnumerable<CountryInfo> _Contries;
+
+
+        /// <summary> Статистика по странам</summary>
+        public IEnumerable<CountryInfo> Contries
+        {
+            get=> _Contries;
+          private  set => Set(ref _Contries, value);
+        }
+
+        #endregion
+
+        #region Команды
+        public ICommand RefreshDataCommand { get; }
+
+        private void OnRefreshDataCommandExecuted(object p)
+        {
+            Contries = _DataService.GetData();
+        }
+
+        #endregion
 
         public CountriesStatisticViewModel(MainWindowViewModel MainModel)
         {
-            _MainModel = _MainModel;
+            MainModel = MainModel;
+            _DataService = new DataService();
+
+            #region Команды
+
+            RefreshDataCommand = new LambdaCommand(OnRefreshDataCommandExecuted);
+
+            #endregion
         }
     }
 }
