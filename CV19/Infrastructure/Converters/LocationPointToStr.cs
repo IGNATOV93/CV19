26c959cec66e4f84e7ce38f72e5c5fef83500ca4
IGNATOV93 <ignatowja@gmail.com>
2023-02-06 01:14:54 +0300

    Создания конвертера Point

---
+++ CV19/Infrastructure/Converters/LocationPointToStr.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Globalization;
+using System.Windows;
+
+
+namespace CV19.Infrastructure.Converters
+{
+    internal class LocationPointToStr:IValueConverter
+    {
+        public object Convert(object value, Type t, object p, CultureInfo c)
+        {
+            if(!(value is Point point)) return null;
+            return $"Lat:{point.X};Lon:{point.Y}";
+        }
+
+        public object ConvertBack(object value, Type y, object p, CultureInfo c)
+        {
+            if (!(value is string str)) return null;
+           var components = str.Split(';');
+           var lat_str = components[0].Split(':')[1];
+           var lon_str = components[1].Split(':')[1];
+
+           var lat = double.Parse(lat_str);
+           var lon = double.Parse(lon_str);
+           return new Point(lat, lon);
+        }
+    }
+}
