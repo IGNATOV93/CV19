d3e51d9cf9bc4b7b6a7e3b19250244853e623379
IGNATOV93 <ignatowja@gmail.com>
2023-02-13 00:01:48 +0300

    Добавили мультиконвертер

---
+++ CV19/Infrastructure/Converters/MultiConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace CV19.Infrastructure.Converters
+{
+    internal abstract class MultiConverter : IMultiValueConverter
+    {
+        public abstract object Convert(object[] vv, Type t, object p, CultureInfo c);
+
+        public virtual object[] ConvertBack(object v, Type[] tt, object p, CultureInfo c) =>
+            throw new NotSupportedException("Обратное преобразование не поддерживается !");
+    }
+}
---
+++ CV19/Infrastructure/Converters/ToArray.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV19.Infrastructure.Converters
+{
+    internal class ToArray:MultiConverter
+    {
+        public override object Convert(object[] vv, Type t, object p, CultureInfo c) => vv;
+        //public override object[] ConvertBack(object v, Type[] tt, object p, CultureInfo c)=> v as object[];
+    }
+}
