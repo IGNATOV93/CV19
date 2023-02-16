d8f494c1044b2d83e12762b8d6dbf483af7ae939
IGNATOV93 <ignatowja@gmail.com>
2023-02-16 20:44:28 +0300

    Мультиконвертер

--- CV19/Infrastructure/Converters/ToArray.cs
+++ CV19/Infrastructure/Converters/ToArray.cs
@@ -4,12 +4,19 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 
 namespace CV19.Infrastructure.Converters
 {
     internal class ToArray:MultiConverter
     {
-        public override object Convert(object[] vv, Type t, object p, CultureInfo c) => vv;
+        public override object Convert(object[] vv, Type t, object p, CultureInfo c)
+        {
+            var collection = new CompositeCollection();
+            foreach (var v in vv)
+                collection.Add(v);
+            return collection;
+        }
         //public override object[] ConvertBack(object v, Type[] tt, object p, CultureInfo c)=> v as object[];
     }
 }
