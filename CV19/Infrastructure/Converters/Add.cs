c290e87ce73e473c84ca19538958fed4c20d0d74
IGNATOV93 <ignatowja@gmail.com>
2023-02-18 18:08:18 +0300

    все работает на данный момент

--- CV19/Infrastructure/Converters/Add.cs
+++ CV19/Infrastructure/Converters/Add.cs
@@ -4,37 +4,33 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Data;
 using System.Windows.Markup;
 
 namespace CV19.Infrastructure.Converters
 {
-    [MarkupExtensionReturnType(typeof(Ratio))]
-    internal class Ratio : Converter
+    [MarkupExtensionReturnType(typeof(Add))]
+    internal class Add : Converter
 
     {
         [ConstructorArgument("K")]
-        public double K { get; set; } = 1;
+        public double B { get; set; } = 1;
 
-        public Ratio() {}
+        public Add() { }
 
-        public Ratio(double K) => this.K = K;
+        public Add(double K) => this.B = K;
 
         public override object Convert(object value, Type t, object p, CultureInfo c)
         {
             if (value is null) return null;
-            var x = System.Convert.ToDouble(value,c);
-            return x * K;
+            var x = System.Convert.ToDouble(value, c);
+            return x + B;
         }
 
         public override object ConvertBack(object value, Type t, object p, CultureInfo c)
         {
             if (value is null) return null;
             var x = System.Convert.ToDouble(value, c);
-            return x / K;
+            return x - B;
         }
     }
-
-
-    
 }
